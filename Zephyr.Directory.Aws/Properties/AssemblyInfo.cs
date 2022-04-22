d996ec141793d4da8da359c4b50c0c5cfe364ef6
Guy Waguespack <guy.waguespack@gmail.com>
2022-04-22 16:12:38 -0500

    Added DLL and CloudFormation Template Versioning

---
+++ Zephyr.Directory.Aws/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Zephyr.Directory.Aws")]
+[assembly: AssemblyDescription("AWS Implemention of MyriAD")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zephyr.Directory.Aws")]
+[assembly: AssemblyCopyright("Copyright © Guy Waguespack, 2019 - 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.22112.0")]
+[assembly: AssemblyFileVersion("1.0.22112.0")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
